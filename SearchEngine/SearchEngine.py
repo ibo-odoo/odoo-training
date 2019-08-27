ff4225267cacab568c6dbf3c20778fe810b2a555
Ipsita Borisagar <ibo@odoo.com>
2019-08-27 14:59:14 +0530

    adding session with visited link

--- SearchEngine/SearchEngine.py
+++ SearchEngine/SearchEngine.py
@@ -1,18 +1,15 @@
 import os
 import json
+import secrets
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
 from jinja2 import Environment, FileSystemLoader
-from http import cookies
 
 
 class SearchEngine(object):
-
-    def dispatch_request(self, request):
-        return Response('Hello World!')
 
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
@@ -23,73 +20,73 @@ class SearchEngine(object):
         return self.wsgi_app(environ, start_response)
 
     def __init__(self):
-    
+        self.session={}
         template_path = os.path.join(os.path.dirname(__file__), 'template')
     
         self.jinja_env = Environment(loader=FileSystemLoader(template_path),
                                  autoescape=True)  
         self.url_map = Map([
             Rule('/', endpoint='index_url'),
-            Rule('/search_controller', endpoint='search_controller')
-            #Rule('/link_color', endpoint='link_color')
+            Rule('/search_controller', endpoint='search_controller'),
+            Rule('/visit', endpoint='visit')
             ])
     
     
     def index_url(self, request):
+        
+        sid = request.cookies.get('session')
+        visited_links = self.session.get('visited_links') or []
+        print('sid', sid)
+        if sid is None:
+            sid = self.make_token()
+            visited_links = []
+        self.session.update({
+            'session_id': sid,
+            'visited_links': visited_links
+            })
         with open('static/app.json') as response:
             data = json.loads(response.read())
-            #data = json.load(source)
+          
 
-        return self.render_template("page.html",data=data)
+        response = self.render_template("page.html",data=data)
+        response.set_cookie('session', sid)
+        return response
 
     def search_controller(self, request):
-
+       
         keyworddata = request.args.get('searchkey')
         with open('static/app.json') as response:
-            data = json.loads(response.read())
-            #data = json.load(source)
-        #data = json.loads(keyword) 
+            data = json.loads(response.read()) 
         mykeydata=[]
+
         for i in data['result']:
-            #import pdb;pdb.set_trace()
+            i.update({'visited': 'false'})
             if keyworddata in i['keyword']:
-               mykeydata.append(i)
-               print(mykeydata)
-            
+                if i.get('link') in self.session.get('visited_links'):
+                    i.update({'visited': 'true'})
+                mykeydata.append(i)
+                
         return Response(json.dumps(mykeydata),mimetype='application/json')
 
-
-    def link_color(self, request):
-        myquestion=request.args.get('myque')
-        #Get cookies
-        cookie_data=request.cookie.get('app_cookie')
-        searched_data=[]
-        if cookie_data:
-            #Read cookies
-            cookie=json.loads(cookie_data)
-            print(cookie)
-            #set cookies
-        if cookie.exist:
-
-        else:
-            response.set_cookie('app_cookie', cookie_data,
-                            httponly=True)
+    def visit(self, request):
         
-        with open('static/app.json') as response:
-            data = json.loads(response.read())
-        
-        for i in data['result']:
-            if i[0] in cookie:
-                searched_data.append(i)
-                print(searched_data)
-
-        return Response('app_cookie',json.dumps(searched_data))
+        print('url', request.args.get('url'))
+        target_url = request.args.get('url')
+        visited_links = self.session.get('visited_links')
+        print('visited_links', visited_links, type(visited_links))
+        if target_url not in visited_links:
+            visited_links.append(target_url)
+        self.session.update({
+            'visited_links': visited_links
+            })
+        return redirect(target_url)
 
 
     def dispatch_request(self, request):
         adapter = self.url_map.bind_to_environ(request.environ)
         try:
             endpoint, values = adapter.match()
+            print('endpoint', endpoint)
             return getattr(self, endpoint)(request, **values)
         except HTTPException as e:
             return e    
@@ -97,6 +94,9 @@ class SearchEngine(object):
     def render_template(self, templatename, **context):
         t = self.jinja_env.get_template(templatename)
         return Response(t.render(context), mimetype='text/html')
+
+    def make_token(self):
+        return secrets.token_urlsafe(16)
     
 
 def create_app():
@@ -110,8 +110,3 @@ if __name__ == '__main__':
     from werkzeug.serving import run_simple
     app = create_app()
     run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
-
-
-
-
-
